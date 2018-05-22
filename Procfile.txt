e0d42e9d8231ae771ec96c74dccd16dbe5d95f55
metol007q <39251082+metol007q@users.noreply.github.com>
2018-05-21 19:57:32 -0500

    Add files via upload

---
+++ Procfile.txt
@@ -0,0 +1,1 @@
+worker: python metols_loyal_servent.py
---
+++ metols_loyal_servent - Copy.py
@@ -0,0 +1,23 @@
+#Bot by metol007
+
+import discord
+from discord.ext import commands
+from discord.ext.commands import bot
+
+bot = commands.Bot(command_prefix="")
+
+@bot.event
+async def on_ready():
+	print ("Ready for your commands master")
+	print ("I am running on"+bot.user.name)
+	print ("With the ID:"+bot.user.id)
+	
+@bot.command(pass_context=True)
+async def ping(ctx):
+	await bot.say("pong")
+	
+@bot.command(pass_context=True)
+async def fuck(ctx):
+	await bot.say("Ill fuck you anytime")
+
+bot.run("NDQ1MDY0ODE4NDEyNjE3NzI5.DdlUQA.Zsrgcwm0cinKkp9-TRhTVlhygVc")
