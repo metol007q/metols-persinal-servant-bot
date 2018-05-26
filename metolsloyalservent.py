5a685e75d3e3f62e959527a02036508150c42b07
metol007q <39251082+metol007q@users.noreply.github.com>
2018-05-25 23:27:59 -0500

    Update metolsloyalservent.py

--- metolsloyalservent.py
+++ metolsloyalservent.py
@@ -20,4 +20,4 @@ async def ping(ctx):
 async def fuck(ctx):
 	await bot.say("Ill fuck you anytime")
 
-bot.run(os.environ.["TOKEN"])
+bot.run("NDQ1MDY0ODE4NDEyNjE3NzI5.DdlUQA.Zsrgcwm0cinKkp9-TRhTVlhygVc")
